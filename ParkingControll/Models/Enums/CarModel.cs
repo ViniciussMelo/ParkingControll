9e47174acb0ba603a4864daa09ef0c2abb4a0c38
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-09-30 12:33:07 -0300

    Add: Views, controllers for the classess

--- ParkingControll/Models/Enums/CarModel.cs
+++ ParkingControll/Models/Enums/CarModel.cs
@@ -1,56 +1,108 @@
-﻿namespace ParkingControll.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingControll.Models.Enums
 {
-    public enum CarModel
+    public enum CarModel : int
     {
+        [Display(Name = "Onix")]
         Onix,
+        [Display(Name = "Ka")]
         Ka,
+        [Display(Name = "HB20")]
         HB20,
+        [Display(Name = "OnixPlus")]
         OnixPlus,
+        [Display(Name = "Argo")]
         Argo,
+        [Display(Name = "Gol")]
         Gol,
+        [Display(Name = "Strada")]
         Strada,
+        [Display(Name = "Renegade")]
         Renegade,
+        [Display(Name = "TCross")]
         TCross,
+        [Display(Name = "Kwid")]
         Kwid,
+        [Display(Name = "Polo")]
         Polo,
+        [Display(Name = "Corolla")]
         Corolla,
+        [Display(Name = "KaSedan")]
         KaSedan,
+        [Display(Name = "Toro")]
         Toro,
+        [Display(Name = "Kicks")]
         Kicks,
+        [Display(Name = "Mobi")]
         Mobi,
+        [Display(Name = "Creta")]
         Creta,
+        [Display(Name = "Sandero")]
         Sandero,
+        [Display(Name = "Compass")]
         Compass,
+        [Display(Name = "Hilux")]
         Hilux,
+        [Display(Name = "HRV")]
         HRV,
+        [Display(Name = "Saveiro")]
         Saveiro,
+        [Display(Name = "Virtus")]
         Virtus,
+        [Display(Name = "EcoSport")]
         EcoSport,
+        [Display(Name = "Tracker")]
         Tracker,
+        [Display(Name = "HB20S")]
         HB20S,
+        [Display(Name = "YarisHB")]
         YarisHB,
+        [Display(Name = "S10")]
         S10,
+        [Display(Name = "Ranger")]
         Ranger,
+        [Display(Name = "Cronos")]
         Cronos,
+        [Display(Name = "Versa")]
         Versa,
+        [Display(Name = "Fox")]
         Fox,
+        [Display(Name = "Civic")]
         Civic,
+        [Display(Name = "Voyage")]
         Voyage,
+        [Display(Name = "Fiorino")]
         Fiorino,
+        [Display(Name = "Spin")]
         Spin,
+        [Display(Name = "Fit")]
         Fit,
+        [Display(Name = "Logan")]
         Logan,
+        [Display(Name = "Tiggo5X")]
         Tiggo5X,
+        [Display(Name = "YarisSedan")]
         YarisSedan,
+        [Display(Name = "Amarok")]
         Amarok,
+        [Display(Name = "Captur")]
         Captur,
+        [Display(Name = "Uno")]
         Uno,
+        [Display(Name = "Duster")]
         Duster,
+        [Display(Name = "Siena")]
         Siena,
+        [Display(Name = "EtiosHB")]
         EtiosHB,
+        [Display(Name = "Up")]
         Up,
+        [Display(Name = "C4Cactus")]
         C4Cactus,
+        [Display(Name = "Montana")]
         Montana,
+        [Display(Name = "WR")]
         WR,
     }
 }
--- ParkingControll/Models/Enums/RecordStatus.cs
+++ ParkingControll/Models/Enums/RecordStatus.cs
@@ -1,8 +1,12 @@
-﻿namespace ParkingControll.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingControll.Models.Enums
 {
     public enum RecordStatus : int
     {
+        [Display(Name = "Inactive")]
         INATIVE = 0,
+        [Display(Name = "Active")]
         ACTIVE = 1
     }
 }
